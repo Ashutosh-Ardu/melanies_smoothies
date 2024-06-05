0c8e96fc262c2d92585eb0cf4d575374c0d8434a
Ashutosh A <96434947+Ashutosh-Ardu@users.noreply.github.com>
2024-06-05 15:05:33 +0530

    Update streamlit_app.py
    space added

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if ingredients_list:
     
     for i in ingredients_list:
         ingredients_string += i + " "
-        st.subheader(i + 'Nutrition Information')
+        st.subheader(i + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + i)
         fv_df =  st.dataframe(data = fruityvice_response.json(),use_container_width=True)
     # st.write(ingredients_string)
