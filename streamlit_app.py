6e61fa8bf560f5a6ba8c5f2c4df0635fd3e8d3cd
Ashutosh A <96434947+Ashutosh-Ardu@users.noreply.github.com>
2024-06-05 15:01:58 +0530

    Update streamlit_app.py
    Fruityvice api call position in code changed inside the ingredients for loop.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fv_df =  st.dataframe(data = fruityvice_response.json(),use_container_width=True)
-
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -36,6 +33,8 @@ if ingredients_list:
     
     for i in ingredients_list:
         ingredients_string += i + " "
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df =  st.dataframe(data = fruityvice_response.json(),use_container_width=True)
     # st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
